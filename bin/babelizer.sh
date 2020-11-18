ac7d4ee25d51a721d945fdf301f26519fd6cf788
Elsa Culler <eculler@gmail.com>
2020-11-18 14:12:20 -0700

    docker fixes

--- bin/babelizer.sh
+++ bin/babelizer.sh
@@ -1,11 +1,11 @@
-LOCALWORKDIR=~/Documents/research/dhsvm-csdms
+LOCALWORKDIR=~/Documents/research/dhsvm-csdms/DHSVM-for-CSDMS
 
 # Build container
-cd ${LOCALWORKDIR} && docker build --tag=hydromet ${LOCALWORKDIR}
+cd ${LOCALWORKDIR} && docker build --tag=babelizer ${LOCALWORKDIR}
 
 # Start container, run python script
 set -f && docker run --rm -it \
   --security-opt seccomp=unconfined \
-  -t hydromet:latest \
-  conda run -n hydrometenv \
-  python
+  -t babelizer:latest \
+  /bin/bash #conda run -n wrap \
+  #test -f $CONDA_PREFIX/include/bmi_heat.h
