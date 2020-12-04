52e7211635249c74d1a29d823ac74b36a205af60
slundell123 <salu3240@colorado.edu>
2020-12-03 20:54:51 -0700

    adding babalizer script

--- bin/babelizer.sh
+++ bin/babelizer.sh
@@ -1,10 +1,10 @@
-LOCALWORKDIR=~/Documents/research/dhsvm-csdms/DHSVM-for-CSDMS
+LOCALWORKDIR=~/Documents/DHSVM-for-CSDMS
 
 # Build container
-cd ${LOCALWORKDIR} && docker build --tag=babelizer ${LOCALWORKDIR}
+cd ${LOCALWORKDIR} && sudo docker build --tag=babelizer ${LOCALWORKDIR}
 
 # Start container, run python script
-set -f && docker run --rm -it \
+set -f && sudo docker run --rm -it \
   --security-opt seccomp=unconfined \
   -t babelizer:latest \
   /bin/bash #conda run -n wrap \
