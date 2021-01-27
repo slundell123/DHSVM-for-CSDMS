b410db0f87fd74660d7c8c7c480c80cc84e44547
slundell123 <salu3240@colorado.edu>
2021-01-26 17:20:42 -0700

    Adding working dockerfile and babelizer example

---
+++ cxxExampleDocker/bin/babelizer.sh
@@ -0,0 +1,12 @@
+LOCALWORKDIR=~/Documents/DLA/DHSVM-for-CSDMS/cxxExampleDocker
+
+
+# Build container
+cd ${LOCALWORKDIR} && sudo docker build --tag=babelizer ${LOCALWORKDIR}
+
+# Start container, run python script
+set -f && sudo docker run --rm -it \
+  --security-opt seccomp=unconfined \
+  -t babelizer:latest \
+  /bin/bash #conda run -n wrap \
+  #test -f $CONDA_PREFIX/include/bmi_heat.h
