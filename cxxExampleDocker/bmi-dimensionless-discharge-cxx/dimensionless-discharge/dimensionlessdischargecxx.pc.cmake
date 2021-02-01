6996f947838fdc14b4a948aa6e3ed43c071b9ca7
slundell123 <salu3240@colorado.edu>
2021-02-01 11:57:22 -0700

    Renaming files and some variables
    variable changes in former heat.cxx and heat.hxx files

---
+++ cxxExampleDocker/bmi-dimensionless-discharge-cxx/dimensionless-discharge/dimensionlessdischargecxx.pc.cmake
@@ -0,0 +1,5 @@
+Name: DimensionlessDischargeCXX
+Description: 2D dimensionless-discharge Equation
+Version: ${DimensionlessDischarge_VERSION}
+Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmidimensionlessdischargecxx
+Cflags: -I${CMAKE_INSTALL_PREFIX}/include
--- cxxExampleDocker/bmi-dimensionless-discharge-cxx/heat/heatcxx.pc.cmake
+++
@@ -1,5 +0,0 @@
-Name: HeatCXX
-Description: 2D Heat Equation
-Version: ${HEAT_VERSION}
-Libs: -L${CMAKE_INSTALL_PREFIX}/lib -lbmiheatcxx
-Cflags: -I${CMAKE_INSTALL_PREFIX}/include
